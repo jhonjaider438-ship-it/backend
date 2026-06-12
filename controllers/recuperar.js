e4da5920db5693f9b5f0976705eac66d1074feda
jhonjaider438 <jhonjaider438@gmail.com>
2026-06-12 07:29:05 -0500

    finalizacion de recuperar contraseña

--- controllers/recuperar.js
+++ controllers/recuperar.js
@@ -26,7 +26,31 @@ export const recuperarr = async (req,res) => {
         }
 
         // generamos el codigo de recuperacion
-       const codigo = Math.floor(100000 + Math.random() * 900000).toString(); // codigo de 6 digitos  
+       const codigo = Math.floor(100000 + Math.random() * 900000).toString(); // codigo de 6 digitos
+
+       // guardar el codigo en la base de datos
+       const {error: errorcodigo} = await codigorecupera (usuario.id, codigo);
+       if (errorcodigo) {
+        return res.status(500).json({error:'error al generar el codigo de recuperacion'});
+       }
+       // creamos el email del codigo
+       await transporte.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: `tu codigo de recuperacion es: ${codigo}`,
+        html:`
+        <h2> recuperacin de contraseña </h2>
+        <p> hola ${usuario.nombre || 'usuario'}, </p>
+        <p> tu codigo de recuperacion es: </p>
+        <h1 style="color: #39a; font-size: 36px;">${codigo}</h1>
+        <p> este codigo es valido por 15 minutos si no lo solisitaste ignora este correo. </p>
+        <p>grasias</p>
+        <p> el equipode soporte </p>
+        <p> no compartas este codigo con nadie </p>
+        `
+       });
+       return res.status(200).json({ message: 'codigo de recuperacion enviado al correo electronico'});
+       
     }catch (error) {
         console.error('Error en forgotPassword:', error);
         return res.status(500).json({ error: 'Error al procesar la solicitud' });
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { registro, login  } from '../controllers/autt.js';
+import { recuperarr } from '../controllers/recuperar.js';
 
 const router = express.Router();
 
 // rutas de autenticacion
 router.post('/register', registro);
 router.post('/login', login);
+
+// ruta para recuperar contraseña
+router.post('/forgot-password', recuperarr);
 
 export default router
